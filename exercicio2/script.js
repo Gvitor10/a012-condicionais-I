684229596890cb67de194fb36de2f8f515e187bb
Gvitor10 <gvitor7062@gmail.com>
2022-08-04 19:39:39 -0300

    Resolviodo os exercicios 1,2 e 3

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 21;
+let isMaiorIdade = true;
+let isCursandoOutraFaculdade = false;
 
-if(){
+if (idade >= 18){
+    console.log('A pessoa é maior de idade');
+} else {
+    console.log('A pessoa é menor de idade');
+}
 
-}else{
-	
+if (isMaiorIdade == true){
+    console.log('A pessoa terminou o ensino medio');
+} else {
+    console.log('A pessoa nao terminou o ensino medio')
+}
+
+if (isCursandoOutraFaculdade == true){
+    console.log('A pessoa esta cursando uma faculdade');
+} else {
+    console.log('A pessoa nao esta cursando uma faculdade');
 }
